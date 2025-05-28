ac3cf072864bdf33a1385098419ed0bc14d5d71a
pkwangshl <57863742+pkwangshl@users.noreply.github.com>
2025-05-29 00:13:39 +0900

    Update and rename get_chat_id.py to

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,14 +1,1 @@
-import os
-import requests
 
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-offset = 0
-while True:
-    resp = requests.get(f"https://api.telegram.org/bot{TOKEN}/getUpdates?offset={offset}").json()
-    for result in resp.get("result", []):
-        message = result.get("message", {})
-        chat = message.get("chat", {})
-        print("chat_id:", chat.get("id"), "message:", message.get("text"))
-        offset = result["update_id"] + 1
-    if not resp.get("result"):
-        break
